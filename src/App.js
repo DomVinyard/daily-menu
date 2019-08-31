f9086c61c5e7ed750558299a8fe4ccb8e8035bd9
dom <domvinyard@me.com>
2019-08-31 15:14:36 +0100

    date

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ const meals = ["breakfast", "lunch", "dinner"]
 const days = ["mon", "tue", "wed", "thur", "fri"]
 const foods = ["junk", "mid", "healthy"] // public/images/junk.png
 
-const App = () => {
-  const [healthiness, setHealthiness] = useState(0) // from -1 to 1
+export default () => {
   const [week, setWeek] = useState(1)
-  const navStyle = { fontSize: "1.8rem", margin: "0 8px", cursor: "pointer" }
+  const [target, setTarget] = useState(0) // from -1 to 1
   return (
     <div
       style={{
@@ -20,74 +19,96 @@ const App = () => {
       }}
     >
       <div>
-        <h1 style={{ marginBottom: "3rem", "user-select": "none" }}>
-          <span
-            style={{
-              opacity: week === 1 ? 0.1 : 1,
-              pointerEvents: week === 1 ? "none" : "all",
-              ...navStyle
-            }}
-            onClick={() => setWeek(Math.max(0, week - 1))}
-            direction={-1}
-            children="⏪"
-          />
-          <span>Week {week}</span>
-          <span
-            style={navStyle}
-            onClick={() => setWeek(week + 1)}
-            direction={1}
-            children={"⏩"}
-          />
-        </h1>
-        <Options setHealthiness={setHealthiness}></Options>
-        <Grid healthiness={healthiness} week={week}></Grid>
+        <Header week={week} setWeek={setWeek} />
+        <Options setTarget={setTarget} />
+        <Grid target={target} week={week} />
       </div>
     </div>
   )
 }
 
-const Options = ({ setHealthiness }) => (
+const Header = ({ week, setWeek }) => {
+  const navStyle = { fontSize: "1.8rem", margin: "0 8px", cursor: "pointer" }
+  return (
+    <h1 style={{ marginBottom: "3rem", "user-select": "none" }}>
+      <span
+        style={{
+          opacity: week === 1 ? 0.1 : 1,
+          pointerEvents: week === 1 ? "none" : "all",
+          ...navStyle
+        }}
+        onClick={() => setWeek(Math.max(0, week - 1))}
+        direction={-1}
+        children="⏪"
+      />
+      <span>Week {week}</span>
+      <span
+        style={navStyle}
+        onClick={() => setWeek(week + 1)}
+        direction={1}
+        children={"⏩"}
+      />
+    </h1>
+  )
+}
+
+const Options = ({ setTarget }) => (
   <div style={{ width: 420, margin: "2rem auto" }}>
     <Slider
       defaultValue={50}
       valueLabelFormat={value => `${(value / 50 - 1).toFixed(1)}`}
-      onChange={(_, value) => setHealthiness(value / 50 - 1)}
+      onChange={(_, value) => setTarget(value / 50 - 1)}
       valueLabelDisplay="off"
     />
   </div>
 )
 
-const Grid = ({ healthiness, week }) => (
-  <div style={{ display: "flex", marginTop: 16, justifyContent: "center" }}>
+const Grid = ({ target, week }) => (
+  <div
+    style={{
+      display: "flex",
+      marginTop: 16,
+      justifyContent: "center",
+      fontWeight: "bold"
+    }}
+  >
+    <div style={{ marginTop: 0 }}>
+      {days.map(day => (
+        <div style={{ textAlign: "right", margin: "80px 0 126px 0" }}>
+          {day}
+        </div>
+      ))}
+    </div>
     {meals.map(meal => (
       <div key={meal} style={{ display: "flex", flexDirection: "column" }}>
-        <div style={{ textAlign: "center", fontWeight: "bold" }}>{meal}</div>
-        {days.map(day => {
-          // set a threshold for each item
-          const threshold = foods
-            .map(food => seedrandom(`${week}.${day}.${meal}.${food}`)() * 2 - 1)
-            .slice(1)
-            .map(t => healthiness >= t)
-            .filter(Boolean).length
-          const foodItem = foods[threshold]
-          return (
-            <div
-              key={meal + day}
-              style={{
-                margin: 8,
-                height: 128,
-                width: 128,
-                backgroundImage: `url(./images/${foodItem}.png)`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center"
-              }}
-            ></div>
-          )
-        })}
+        <div style={{ textAlign: "center" }}>{meal}</div>
+        {days.map(day => (
+          <GridItem week={week} day={day} meal={meal} target={target} />
+        ))}
       </div>
     ))}
   </div>
 )
 
-export default App
+const GridItem = ({ week, day, meal, target }) => {
+  const threshold = foods
+    .map(food => seedrandom(`${week}.${day}.${meal}.${food}`)() * 2 - 1)
+    .slice(1)
+    .map(t => target >= t)
+    .filter(Boolean).length
+  const foodItem = foods[threshold]
+  return (
+    <div
+      key={meal + day}
+      style={{
+        margin: 8,
+        height: 128,
+        width: 128,
+        backgroundImage: `url(./images/${foodItem}.png)`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center"
+      }}
+    ></div>
+  )
+}
